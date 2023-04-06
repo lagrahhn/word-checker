c0c596620d5cff6fdf3afa181e438561fea16a34
老马啸西风 <houbinbin.echo@gmail.com>
2023-04-06 10:31:52 +0800

    删除不必要的文件

---
+++ src/main/java/com/github/houbb/word/checker/Main.java
@@ -0,0 +1,11 @@
+package com.github.houbb.word.checker;
+
+import com.github.houbb.word.checker.util.WordCheckerHelper;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(WordCheckerHelper.correct("speling"));
+
+    }
+}
